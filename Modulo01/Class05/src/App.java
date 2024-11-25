e7831e4f224a4fcd9f97e2b7726dd56d9582ccdf
DanielMateoAriasOrtega <dmateo.arias@gmail.com>
2024-11-25 05:09:52 -0500

    Programming

---
+++ Modulo01/Class05/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+   /*      // while
+        // inicializacion
+        var counter = 1;
+        while (counter <= 10) {
+            System.out.println(counter);
+            counter++;
+        }
+
+        // do while
+        // inicializacion
+    } */
+
+
+    
+    }
+}
